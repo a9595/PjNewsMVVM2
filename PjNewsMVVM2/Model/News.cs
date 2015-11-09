1ad927995eb45a8225c1998081f442e11bcd9128
Andrii Kovalchuk <tieorange@gmail.com>
2015-11-09 14:29:31 +0100

    news file added

---
+++ PjNewsMVVM2/Model/News.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using PjNewsMVVM2.Data;
+
+namespace PjNewsMVVM2.Model
+{
+    public class News
+    {
+        public News()
+        {
+        }
+
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("results")]
+        public IList<NewsResult> Results { get; set; }
+
+        [JsonProperty("cookies")]
+        public IList<string> Cookies { get; set; }
+
+        [JsonProperty("connectorVersionGuid")]
+        public string ConnectorVersionGuid { get; set; }
+
+        [JsonProperty("connectorGuid")]
+        public string ConnectorGuid { get; set; }
+
+        [JsonProperty("pageUrl")]
+        public string PageUrl { get; set; }
+
+        [JsonProperty("outputProperties")]
+        public IList<OutputProperty> OutputProperties { get; set; }
+
+        public string GetAllNewsTitlesString()
+        {
+            string result = "";
+
+            foreach (var rez in Results)
+            {
+                result = result + rez.Date + "  " + rez.LinkText + "\n";
+            }
+
+            return result;
+        }
+
+
+    }
+
+    public class NewsResult
+    {
+        public NewsResult()
+        {
+        }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("link/_text")]
+        public string LinkText { get; set; }
+
+        [JsonProperty("link/_source")]
+        public string LinkSource { get; set; }
+
+        [JsonProperty("link/_title")]
+        public string LinkTitle { get; set; }
+
+        public ArticleAlternative ArticleItem { get; set; }
+
+        public override string ToString()
+        {
+            return Date + "  " + LinkText;
+        }
+    }
+
+    public class OutputProperty
+    {
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    
+}
