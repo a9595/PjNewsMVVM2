738ae949695e26e672692853ee447571a5ae4535
Andrii Kovalchuk <tieorange@gmail.com>
2015-11-09 14:21:51 +0100

    init commit Model Data

---
+++ PjNewsMVVM2/Data/AlternativeArticle.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PjNewsMVVM2.Data
+{
+    public class Result
+    {
+        public Result()
+        {
+            //TODO:add images support
+            //Image = new List<string>();
+            //ImageSource = new List<string>();
+            //ImageAlt = new List<string>(); 
+        }
+
+        //TODO:add images support
+        //[JsonProperty("image/_alt")]
+        //public List<string> ImageAlt { get; set; }
+
+        //[JsonProperty("image")]
+        //public List<string> Image { get; set; }
+
+        //[JsonProperty("image/_source")]
+        //public List<string> ImageSource { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+
+    public class ArticleAlternative
+    {
+        public ArticleAlternative()
+        {
+        }
+
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        //[JsonProperty("results")]
+        //public Result Results { get; set; }
+
+        [JsonProperty("results")]
+        public IList<Result> Results { get; set; }
+
+        [JsonProperty("cookies")]
+        public IList<string> Cookies { get; set; }
+
+        [JsonProperty("connectorVersionGuid")]
+        public string ConnectorVersionGuid { get; set; }
+
+        [JsonProperty("connectorGuid")]
+        public string ConnectorGuid { get; set; }
+
+        [JsonProperty("pageUrl")]
+        public string PageUrl { get; set; }
+
+        [JsonProperty("outputProperties")]
+        public IList<OutputPropertyAlternative> OutputProperties { get; set; }
+
+
+
+        #region Methods added by me
+
+        public string GetTitle()
+        {
+            //return Results.Title;
+
+            return Results.First().Title;
+
+        }
+
+        public string GetContent()
+        {
+            //string delimeter = ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,";
+            //results.First().content.Aggregate((i, j) => i + delimeter + j);
+
+            //var allContent = "";
+            //foreach (var str in Results.First().Content)
+            //{
+            //    allContent = allContent + str;
+            //}
+
+            var content = Results.First().Content;
+            return content ?? "no content";
+        }
+
+        //public async Task<BitmapImage> GetHeaderImage()
+        //{
+        //    var httpClient = new HttpClient();
+        //    var contentBytes = await httpClient.GetByteArrayAsync(Results.First().HeaderImg);
+
+
+        //    var ims = new InMemoryRandomAccessStream();
+        //    var dataWriter = new DataWriter(ims);
+        //    dataWriter.WriteBytes(contentBytes);
+        //    await dataWriter.StoreAsync();
+        //    ims.Seek(0);
+
+        //    var bitmap = new BitmapImage();
+        //    bitmap.SetSource(ims);
+
+        //    GetHeaderImageUrl();
+
+        //    return bitmap;
+        //}
+        //public string GetHeaderImageUrl()
+        //{
+        //    return Results.First().HeaderImg;
+        //}
+
+
+        #endregion
+    }
+
+
+
+    public class OutputPropertyAlternative
+    {
+        public OutputPropertyAlternative()
+        {
+        }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
---
+++ PjNewsMVVM2/Data/NewsGrabber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PjNewsMVVM2.Model;
+
+namespace PjNewsMVVM2.Data
+{
+    public class NewsGrabber
+    {
+
+        private static string _urlNews =
+            "https://api.import.io/store/data/ac6a058d-2785-42b6-b1d9-b20c4d48c8f7/_query?input/webpage/url=http%3A%2F%2Fwww.pja.edu.pl%2Faktualnosci%2Fglowna&_user=ac4a2596-0302-46ee-a01a-153a5b50f8bf&_apikey=ac4a2596030246eea01a153a5b50f8bf8d83fcfebeb20555e1c978bf8baa34cc8783b48aa9648c98236227aa39e38c716a3280346535778f39005f54d0a00eb45cdf4387ab49e5af783d95afa60b5c37";
+
+        public static ArticleAlternative GetAlternativeArticleSimply(string url)
+        {
+            string urlPartAPI1 =
+                "https://api.import.io/store/data/40ab96a9-c714-4844-9eb6-20bd86cf8501/_query?input/webpage/url=";
+            string urlPartApiKey3 = "&_user=ac4a2596-0302-46ee-a01a-153a5b50f8bf&_apikey=ac4a2596030246eea01a153a5b50f8bf8d83fcfebeb20555e1c978bf8baa34cc8783b48aa9648c98236227aa39e38c716a3280346535778f39005f54d0a00eb45cdf4387ab49e5af783d95afa60b5c37";
+
+
+
+            string resultUrl = urlPartAPI1 + url + urlPartApiKey3;
+            Uri requestUri = new Uri(resultUrl, UriKind.Absolute);
+
+
+
+            var httpClient = new HttpClient();
+            var payload = httpClient.GetStringAsync(requestUri).Result;
+
+            var sampleResponse = JsonConvert.DeserializeObject<ArticleAlternative>(payload,
+                new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+
+
+            //var sampleResponse = JsonConvert.DeserializeObject<ArticleAlternative>(payload);
+
+
+            return sampleResponse;
+        }
+
+
+        public static News GetNews()
+        {
+            var httpClient = new HttpClient();
+            var payload = httpClient.GetStringAsync(_urlNews).Result;
+
+            var news = JsonConvert.DeserializeObject<News>(payload,
+                new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
+
+            return news;
+        }
+
+
+
+    }
+}
